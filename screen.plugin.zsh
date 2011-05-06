fa59916d9b370846e495b401129c799ffd271db8
Dave Lee <dave@gray101.com>
2011-05-06 14:22:37 -0500

    This is a simple plugin to enable oh-my-zsh to continue using my GNU Screen settings regardless of theme. Since this might actually be useful to people, I've made this its own plugin :P

---
+++ screen.plugin.zsh
@@ -0,0 +1,48 @@
+# camperdave's screen config'ing plugin 
+# Defined as a plugin, so it works with every theme.
+# Hopefully people like that?
+
+autoload -U add-zsh-hook
+
+
+### Magic Functions
+camperdave-screen-set-title(){
+    printf "\033k%s\033\\" "$1"
+}
+
+camperdave-screen-set-hard(){
+    printf  "\033]0;%s\a" "$1"
+}
+
+camperdave-sceen-preexec(){
+    ## Screen-Specific
+    
+    if [[ $TERM == *screen* ]]; then
+        TITLE="${PWD/#$HOME/~}"
+        s=$3;
+        arg=("${(s/ /)s}")
+        cmd=$arg[1]
+        if [[ $cmd == "ssh" ]]; then
+            TITLE=$arg[2]
+        fi
+        
+        camperdave-screen-set-title $TITLE
+        
+        HARD="${$(echo $3 | sed -r 's/^command sudo ([^ ]*) .*/\1/;tx;s/^([^ ]*) +.*/\1/;s/^([^ ]*)$/\1/;:x;q')/#*\/}"
+        camperdave-screen-set-hard $HARD
+    fi
+}
+
+camperdave-sceen-precmd(){
+    ## Screen-Specific
+    if [[ $TERM == *screen* ]]; then
+        TITLE="${PWD/#$HOME/~}"
+        HARD="zsh"
+        set-title $TITLE 
+        set-hard $HARD
+    fi
+}
+
+add-zsh-hook precmd camperdave-sceen-precmd
+
+add-zsh-hook preexec camperdave-sceen-preexec
